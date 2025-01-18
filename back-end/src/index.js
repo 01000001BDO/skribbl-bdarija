a3741227ac8a19d7c66c40f7ebbf7e8f07d99104
01000001BDO <abdelilahellotfi@gmail.com>
2025-01-18 15:18:00 +0100

    +

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://skribbl-bdarija.vercel.app/",
+    origin: ["https://skribbl-bdarija.vercel.app", "https://skribbl-bdarija.vercel.app/"],
     methods: ["GET", "POST"]
   }
 });
